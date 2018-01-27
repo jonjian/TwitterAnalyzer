028fbb4f64bfdbb2fe3b2e93d25106fec658226b
jonjian <jonjian@gmail.com>
2018-01-27 15:09:11 -0800

    feat create tweet schema

--- db/migrations/20180127121438_01_database_schemas.js
+++ db/migrations/20180127121438_01_database_schemas.js
@@ -21,11 +21,21 @@ exports.up = (knex, Promise) => Promise.all([
     table.string('tweet_id', 100).nullable();
     table.timestamps(true, true);
     table.string('created_at', 100).nullable();
-    table.
+    table.string('username', 100).nullable();
+    table.string('text', 100000).nullable();
+    table.string('keyword', 100).nullable();
+    table.integer('sentiment').nullable();
+    table.integer('favourites').nullable();
+    table.integer('retweets').nullable();
+    table.string('language', 100).nullable();
+    table.string('geo', 1000).nullable();
+    table.string('coordinates', 1000).nullable();
+    table.string('timezone', 100).nullable();
   }),
 ]);
 
 exports.down = (knex, Promise) => Promise.all([
   knex.schema.dropTable('auths'),
   knex.schema.dropTable('profiles'),
+  knex.schema.dropTable('tweets'),
 ]);
