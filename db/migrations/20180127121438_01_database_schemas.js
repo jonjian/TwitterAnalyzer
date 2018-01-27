3a3f1e0b4f804dfb05b194d97199986e6d0f65d4
jonjian <jonjian@gmail.com>
2018-01-27 15:44:20 -0800

    feat create tweet db

--- db/migrations/20180127121438_01_database_schemas.js
+++ db/migrations/20180127121438_01_database_schemas.js
@@ -1,27 +1,11 @@
 exports.up = (knex, Promise) => Promise.all([
-  knex.schema.createTableIfNotExists('profiles', (table) => {
-    table.increments('id').unsigned().primary();
-    table.string('first', 100).nullable();
-    table.string('last', 100).nullable();
-    table.string('display', 100).nullable();
-    table.string('email', 100).nullable().unique();
-    table.string('phone', 100).nullable();
-    table.timestamps(true, true);
-  }),
-  knex.schema.createTableIfNotExists('auths', (table) => {
-    table.increments('id').unsigned().primary();
-    table.string('type', 8).notNullable();
-    table.string('oauth_id', 30).nullable();
-    table.string('password', 100).nullable();
-    table.string('salt', 100).nullable();
-    table.integer('profile_id').references('profiles.id').onDelete('CASCADE');
-  }),
   knex.schema.createTableIfNotExists('tweets', (table) => {
     table.increments('id').unsigned().primary();
     table.string('tweet_id', 100).nullable();
     table.timestamps(true, true);
-    table.string('created_at', 100).nullable();
+    table.string('timestamp', 100).nullable();
     table.string('username', 100).nullable();
+    table.string('profile_picture', 1000).nullable();
     table.string('text', 100000).nullable();
     table.string('keyword', 100).nullable();
     table.integer('sentiment').nullable();
